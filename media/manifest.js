2cf6f0c50e52835976f5ebeb37790be7693e604b
crt_ <77174996+8041q@users.noreply.github.com>
2026-05-14 18:17:29 +0100

    Update language switcher logic and refresh options based on available videos; fix media manifest paths

--- media/manifest.js
+++ media/manifest.js
@@ -1,17 +1,12 @@
 ﻿window.KIOSK_MEDIA_MANIFEST = {
     "en":  [
-               "media/en/test.mp4"
            ],
     "pt":  [
-
            ],
     "es":  [
-
            ],
     "fr":  [
-
            ],
     "zh":  [
-
            ]
 }; 
